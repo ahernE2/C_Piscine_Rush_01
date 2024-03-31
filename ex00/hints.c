116cf574a1b4fef49c5812dcf389a9953f0cb190
Alejandro Hernandez Hernandez <alejhern@cbr10s4.42barcelona.com>
2024-03-31 12:09:48 +0200

    Geracion de todas las posibilidades de la matriz/ falta verificar cual es la correcta

---
+++ ex00/hints.c
@@ -0,0 +1,129 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hints.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/31 11:15:22 by alejhern          #+#    #+#             */
+/*   Updated: 2024/03/31 11:54:57 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#define N 4
+
+void	get_row_hints(int board[N][N], int row_hints[N][2])
+{
+	int	i;
+	int	j;
+	int	visible_left;
+	int	visible_right;
+
+	i = 0;
+	while (i < N)
+	{
+		visible_left = 0;
+		visible_right = 0;
+		j = 0;
+		while (j < N)
+		{
+			if (board[i][j] > visible_left)
+			{
+				visible_left++;
+				row_hints[i][0]++;
+			}
+			if (board[i][N - 1 - j] > visible_right)
+			{
+				visible_right++;
+				row_hints[i][1]++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	get_col_hints(int board[N][N], int col_hints[N][2])
+{
+	int	j;
+	int	i;
+	int	visible_up;
+	int	visible_down;
+
+	j = 0;
+	while (j < N)
+	{
+		visible_up = 0;
+		visible_down = 0;
+		i = 0;
+		while (i < N)
+		{
+			if (board[i][j] > visible_up)
+			{
+				visible_up++;
+				col_hints[j][0]++;
+			}
+			if (board[N - 1 - i][j] > visible_down)
+			{
+				visible_down++;
+				col_hints[j][1]++;
+			}
+			i++;
+		}
+		j++;
+	}
+}
+
+
+void print_hints(int hints[N][2], char dir) {
+    char buffer[20]; // Suficiente para almacenar cualquier número entero y caracteres adicionales
+    int i = 0;
+    int len;
+
+    // Escribir la cadena "Pistas para "
+    write(1, "Pistas para ", 12);
+
+    // Escribir el carácter de dirección
+    write(1, &dir, 1);
+
+    // Escribir el carácter ':'
+    write(1, ": ", 2);
+
+    // Convertir e imprimir cada par de coordenadas
+    while (i < N) {
+        // Convertir el primer número del par a una cadena
+        len = sprintf(buffer, "(%d, ", hints[i][0]);
+        write(1, buffer, len);
+
+        // Convertir el segundo número del par a una cadena
+        len = sprintf(buffer, "%d) ", hints[i][1]);
+        write(1, buffer, len);
+
+        i++;
+    }
+
+    // Escribir un salto de línea al final
+    write(1, "\n", 1);
+}
+
+int main() {
+    int board[N][N] = {
+        {1, 2, 3, 4},
+        {2, 3, 4, 1},
+        {3, 4, 1, 2},
+        {4, 1, 2, 3}
+    }; // Tablero de ejemplo
+
+    int row_hints[N][2] = {0};
+    int col_hints[N][2] = {0};
+
+    get_row_hints(board, row_hints);
+    get_col_hints(board, col_hints);
+
+    print_hints(row_hints, 'F');
+    print_hints(col_hints, 'C');
+
+    return 0;
+}
---
+++ ex00/skycrapers_force.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   skycrapers_force.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/31 11:12:31 by alejhern          #+#    #+#             */
+/*   Updated: 2024/03/31 11:19:04 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#define N 4 // Tamaño del tablero (NxN)
+
+void	print_board(int board[N][N]);
+void	get_row_hints(int board[N][N], int row_hints[N][2]);
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_board(int board[N][N])
+{
+	int		i;
+	int		j;
+	char	num_char;
+
+	i = 0;
+	while (i < N)
+	{
+		j = 0;
+		while (j < N)
+		{
+			num_char = board[i][j] + '0';
+			ft_putchar(num_char);
+			if (j < N - 1)
+				ft_putchar(' ');
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int	is_valid(int board[N][N], int row, int col, int num)
+{
+	int	x;
+
+	x = 0;
+	while (x < N)
+	{
+		if (board[row][x] == num || board[x][col] == num)
+		{
+			return (0);
+		}
+		x++;
+	}
+	return (1);
+}
+
+void	generate_skyscrapers(int board[N][N], int row, int col)
+{
+	int	num;
+
+	if (row == N)
+	{
+		print_board(board);
+		return ;
+	}
+	if (col == N)
+	{
+		generate_skyscrapers(board, row + 1, 0);
+		return ;
+	}
+	num = 1;
+	while (num <= N)
+	{
+		if (is_valid(board, row, col, num))
+		{
+			board[row][col] = num;
+			generate_skyscrapers(board, row, col + 1);
+			board[row][col] = 0;
+		}
+		num++;
+	}
+}
+
+int	main(void)
+{
+	int	board[N][N];	
+
+	generate_skyscrapers(board, 0, 0);
+	return (0);
+}
